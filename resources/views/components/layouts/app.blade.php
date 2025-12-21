c061c698b78e07dee3ef264349f50439c38ac406
Mahmudi Shodmehr <mahmudishodmehr@MacBook-Air-Mahmudi.local>
2025-12-21 11:36:05 +0500

    updated

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -16,7 +16,7 @@
         <div class="bg-neutral-800 rounded-xl border-neutral-700 border flex justify-between items-center p-1">
             <flux:avatar src="{{ asset('assets/photo_2025-09-25_23-47-38.png') }}" />
             <div class="text-center">
-                <flux:heading class="uppercase text-xl/5">Shifu Cargo</flux:heading>
+                <flux:heading class="uppercase text-xl/5">Sifat-Cargo</flux:heading>
                 <flux:text class="text-sm/4">transport company</flux:text>
             </div>
 
